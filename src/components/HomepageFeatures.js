ed10bf47fbfbac87dc0570676f56a1b3f557fa5b
Abdulrahman <76847516+abdooo9@users.noreply.github.com>
2022-03-28 21:50:24 +0300

    Image clickable

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -9,11 +9,19 @@ export default function HomepageFeatures() {
 		<center>
 			<img src="https://logs.chat/img/meta.png" className={styles.logo} />
 			<br /><br />
-			<img src="https://nodei.co/npm/logs.chat.png" />
+			<a href="https://www.npmjs.com/package/logs.chat" target="_blank">
+				<img src="https://nodei.co/npm/logs.chat.png" />
+			</a>
 			<br />
-			<img src="https://img.shields.io/npm/v/logs.chat.svg?maxAge=3600" />
+			<a href="https://www.npmjs.com/package/logs.chat" target="_blank">
+				<img src="https://img.shields.io/npm/v/logs.chat.svg?maxAge=3600" />
+			</a>
+
 			&nbsp;
-			<img src="https://img.shields.io/npm/dt/logs.chat.svg?maxAge=3600" />
+			<a href="https://www.npmjs.com/package/logs.chat" target="_blank">
+				<img src="https://img.shields.io/npm/dt/logs.chat.svg?maxAge=3600" />
+			</a>
+
 		</center>
 		<br /><br />
 	</section>
